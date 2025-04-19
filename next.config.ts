fac182b4db58b08516638a9dd4fd48ff4a264e26
Keen Wilson <keenwils@amazon.com>
2025-04-18 17:29:00 -0700

    fix server file

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  generateBuildId: async () => 'build',
   images: {
     unoptimized: true,
-    domains: ['main.d3iu0xoznse3pk.amplifyapp.com'], // Add your Amplify domain
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
-  experimental: {
-    serverActions: true,
-    serverComponentsExternalPackages: ['sharp'],
-    outputFileTracingIncludes: {
-      '/**': ['./public/**/*']
-    }
+    domains: ['main.d3iu0xoznse3pk.amplifyapp.com'],
   }
 }
 
